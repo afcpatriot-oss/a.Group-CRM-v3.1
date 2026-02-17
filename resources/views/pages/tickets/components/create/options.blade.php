f1d292abb20108f4615a308d960c051e55fe907c
afcpatriot-oss <afcpatriot@gmail.com>
2026-02-17 17:58:42 +0200

    Restore hidden default category and priority in create ticket form

--- resources/views/pages/tickets/components/create/options.blade.php
+++ resources/views/pages/tickets/components/create/options.blade.php
@@ -8,74 +8,103 @@
                         {{ cleanLang(__('lang.order_parameters')) }}
                     </div>
                     <div class="x-body form-horizontal">
+
                         @if(auth()->user()->is_team)
+                        <!--client-->
                         <div class="form-group row">
-                            <label for="example-month-input" class="col-12 control-label col-form-label text-left required">{{ cleanLang(__('lang.order_customer')) }}</label>
+                            <label class="col-12 control-label col-form-label text-left required">
+                                {{ cleanLang(__('lang.order_customer')) }}
+                            </label>
                             <div class="col-12">
-                                <select name="ticket_clientid" id="ticket_clientid" class="clients_and_projects_toggle form-control form-control-sm js-select2-basic-search select2-hidden-accessible"
-                                    data-projects-dropdown="ticket_projectid" data-feed-request-type="clients_projects"
-                                    data-ajax--url="{{ url('/') }}/feed/company_names"></select>
+                                <select name="ticket_clientid"
+                                        id="ticket_clientid"
+                                        class="clients_and_projects_toggle form-control form-control-sm js-select2-basic-search"
+                                        data-projects-dropdown="ticket_projectid"
+                                        data-feed-request-type="clients_projects"
+                                        data-ajax--url="{{ url('/') }}/feed/company_names">
+                                </select>
                             </div>
                         </div>
 
                         <!--project-->
                         <div class="form-group row">
-                            <label for="example-month-input" class="col-12 col-form-label text-left">{{ cleanLang(__('lang.order_related_project')) }}</label>
+                            <label class="col-12 col-form-label text-left">
+                                {{ cleanLang(__('lang.order_related_project')) }}
+                            </label>
                             <div class="col-12">
-                                <select class="select2-basic form-control form-control-sm dynamic_ticket_projectid" id="ticket_projectid" name="ticket_projectid"
-                                    disabled>
+                                <select class="select2-basic form-control form-control-sm dynamic_ticket_projectid"
+                                        id="ticket_projectid"
+                                        name="ticket_projectid"
+                                        disabled>
                                 </select>
                             </div>
                         </div>
                         @endif
-                        <!--department-->
-                        <div class="form-group row">
-                            <label for="example-month-input" class="col-12 control-label col-form-label text-left required">{{ cleanLang(__('lang.order_category')) }}</label>
+
+                        <!--department (hidden, value selected & sent)-->
+                        <div class="form-group row" hidden aria-hidden="true">
+                            <label class="col-12 control-label col-form-label text-left required">
+                                {{ cleanLang(__('lang.order_category')) }}
+                            </label>
                             <div class="col-12">
-                                <select class="select2-basic form-control  form-control-sm" id="ticket_categoryid" name="ticket_categoryid">
-                                    <option></option>
+                                <select class="select2-basic form-control form-control-sm"
+                                        id="ticket_categoryid"
+                                        name="ticket_categoryid">
                                     @foreach($categories as $category)
-                                    <option value="{{ $category->category_id }}">
-                                        {{ $category->category_name }}</option>
+                                        <option value="{{ $category->category_id }}"
+                                            {{ !old('ticket_categoryid') && $category->is_default ? 'selected' : '' }}>
+                                            {{ $category->category_name }}
+                                        </option>
                                     @endforeach
                                 </select>
                             </div>
                         </div>
 
-                        <!--clients projects-->
                         @if(auth()->user()->is_client)
+                        <!--clients projects-->
                         <div class="form-group row">
-                            <label for="example-month-input" class="col-12 col-form-label text-left">{{ cleanLang(__('lang.order_related_project')) }}</label>
+                            <label class="col-12 col-form-label text-left">
+                                {{ cleanLang(__('lang.order_related_project')) }}
+                            </label>
                             <div class="col-12">
-                                <select class="select2-basic form-control  form-control-sm" id="ticket_projectid" name="ticket_projectid"
-                                    data-allow-clear="true">
+                                <select class="select2-basic form-control form-control-sm"
+                                        id="ticket_projectid"
+                                        name="ticket_projectid"
+                                        data-allow-clear="true">
                                     <option value=""></option>
                                     @foreach($clients_projects as $project)
-                                    <option value="{{ $project->project_id }}">
-                                        {{ $project->project_title }}</option>
+                                        <option value="{{ $project->project_id }}">
+                                            {{ $project->project_title }}
+                                        </option>
                                     @endforeach
                                 </select>
                             </div>
                         </div>
                         @endif
 
-                        <!--priority-->
-                        @if(auth()->user()->is_team)
-                        <div class="form-group row">
-                            <label for="example-month-input" class="col-12 col-form-label text-left">{{ cleanLang(__('lang.order_priority')) }}</label>
+                        <!--priority (hidden, default=normal)-->
+                        <div class="form-group row" hidden aria-hidden="true">
+                            <label class="col-12 col-form-label text-left">
+                                {{ cleanLang(__('lang.order_priority')) }}
+                            </label>
                             <div class="col-12">
-                                <select class="select2-basic form-control  form-control-sm" id="ticket_priority" name="ticket_priority">
+                                <select class="select2-basic form-control form-control-sm"
+                                        id="ticket_priority"
+                                        name="ticket_priority">
                                     @foreach(config('settings.ticket_priority') as $key => $value)
-                                    <option value="{{ $key }}">{{ runtimeLang($key) }}</option>
+                                        <option value="{{ $key }}"
+                                            {{ $key == 'normal' ? 'selected' : '' }}>
+                                            {{ runtimeLang($key) }}
+                                        </option>
                                     @endforeach
                                 </select>
                             </div>
                         </div>
-                        @endif
 
                         <div class="line m-t-40 m-b-0"></div>
 
                         @include('pages.tickets.components.create.customfields')
+
                     </div>
                 </div>
             </div>
